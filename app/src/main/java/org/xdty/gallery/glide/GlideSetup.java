8e6b68a6d8c43986ffa8d6fc639b366bb5b35686
tianyu <xdtianyu@gmail.com>
2015-12-20 21:48:47 +0800

    update cache size

--- app/src/main/java/org/xdty/gallery/glide/GlideSetup.java
+++ app/src/main/java/org/xdty/gallery/glide/GlideSetup.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.module.GlideModule;
 
 import org.xdty.gallery.model.Media;
@@ -17,6 +18,7 @@ public class GlideSetup implements GlideModule {
 //        builder.setMemoryCache(new LruResourceCache(64*1024*1024));
 //        builder.setBitmapPool(new LruBitmapPool(32 * 1024 * 1024));
         builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, 2147483647));
     }
 
     @Override
