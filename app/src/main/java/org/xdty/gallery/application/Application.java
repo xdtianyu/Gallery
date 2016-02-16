5cc0052930a5e33f94208c93798cf40f8b7754ec
tianyu <xdtianyu@gmail.com>
2016-02-16 21:15:36 +0800

    update setting

--- app/src/main/java/org/xdty/gallery/application/Application.java
+++ app/src/main/java/org/xdty/gallery/application/Application.java
@@ -3,7 +3,6 @@ package org.xdty.gallery.application;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-import org.xdty.gallery.BuildConfig;
 import org.xdty.gallery.R;
 
 import cat.ereza.customactivityoncrash.CustomActivityOnCrash;
@@ -14,7 +13,7 @@ public class Application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean isCatch = pref.getBoolean(getString(R.string.catch_crash_key), BuildConfig.DEBUG);
+        boolean isCatch = pref.getBoolean(getString(R.string.catch_crash_key), false);
         if (isCatch) {
             CustomActivityOnCrash.install(this);
         }
